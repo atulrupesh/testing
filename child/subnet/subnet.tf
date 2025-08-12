3ed729403aca23775b554018d2bf62c8fafd93b9
atulrupesh <rupesh.kumar8612@gmail.com>
2025-08-12 09:27:20 +0530

    Delete child directory

--- child/subnet/subnet.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet-name
-  resource_group_name  = var.rg-name
-  virtual_network_name = var.vnet-name
-  address_prefixes       = var.address_prefixes
-}
-
-
-
